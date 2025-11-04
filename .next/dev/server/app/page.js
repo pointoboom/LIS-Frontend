f31ac2e90107bcc516a42760912c84c97249c865
pointoboom <pointoboom@gmail.com>
2025-11-05 00:34:32 +0700

    redirect from homepage to login

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,5 +1,5 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
-R.c("server/chunks/ssr/51cec_9c28c450._.js")
+R.c("server/chunks/ssr/51cec_next_dist_207fe0cd._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__94474f76._.js")
 R.c("server/chunks/ssr/51cec_77c09f42._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__e8a2741f._.js")
